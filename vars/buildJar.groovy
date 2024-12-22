bca1d591d7d40255a1f5d553503a3782b67ff1b4
Siddhartha536 <siddharthasuman536@gmail.com>
2024-12-22 12:44:25 +0530

    typo fixed

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
-#!/user/bin/env groovy
-
-def call(){
-    echo "Building jar"
-    sh "mvn package"
+def call() {
+    echo "Building jar using Jenkins Maven tool"
+    withMaven(maven: 'maven-3.9') { // Replace 'Maven-3.8.5' with your configured tool name
+        sh "mvn package"
+    }
 }
