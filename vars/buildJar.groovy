0bab8cddf8a888845a025b2c7b27b871555c2e36
Siddhartha536 <siddharthasuman536@gmail.com>
2024-12-22 13:06:10 +0530

    typo fixed

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,4 +1,7 @@
 def call() {
-    echo "Building jar using system Maven"
-    sh "mvn package"
+    echo "Building jar using Maven"
+    def mavenHome = tool name: 'maven-3.9', type: 'maven' // Match the name in Global Tool Configuration
+    env.PATH = "${mavenHome}/bin:${env.PATH}" // Add Maven to the PATH for this script
+    sh "mvn -v" // Verify Maven is accessible
+    sh "mvn package" // Execute Maven package command
 }
